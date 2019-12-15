ee09124a7d968df11afbd161a22553057222aac3
Iaroslav Koshelev <ys_koshelev@Iaroslavs-MacBook-Pro.local>
2019-12-15 18:16:48 +0300

    demo

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from os import system, name
 
 def process_a():
     clear()
-    photo_path = input('Print path to folder with photos stored for encoding')
+    photo_path = input('Print path to folder with photos stored for encoding: ')
     return encode_face(photo_path, photo_path + '/encoding')
 
 def process_b():
     clear()
-    etalon_path = input('Print path to etalon encoding')
+    etalon_path = input('Print path to etalon encoding: ')
     return compare_face_with_etalon(etalon_path)
 
 def process_c():
     clear()
-    etalon_path = input('Print path to etalon encoding')
-    duration = input('Specify duration of test in minutes')
+    etalon_path = input('Print path to etalon encoding: ')
+    duration = input('Specify duration of test in minutes: ')
     return watch_face(etalon_path, 60*float(duration))
 
 def clear(): 
