b9f6128040e319168051390c2c3b3fc0a54c2abe
Iaroslav Koshelev <ys_koshelev@Iaroslavs-MacBook-Pro.local>
2019-12-15 16:49:17 +0300

    compare_faces

--- main.py
+++ main.py
@@ -28,3 +28,36 @@ def encode_face(path_to_photos, path_to_save_encoding):
         return False
     
 
+def compare_face_with_etalon(path_to_etalon_encoding):
+    if running_on_jetson_nano():
+        # Accessing the camera with OpenCV on a Jetson Nano requires gstreamer with a custom gstreamer source string
+        video_capture = cv2.VideoCapture(get_jetson_gstreamer_source(), cv2.CAP_GSTREAMER)
+    else:
+        # Accessing the camera with OpenCV on a laptop just requires passing in the number of the webcam (usually 0)
+        # Note: You can pass in a filename instead if you want to process a video file instead of a live camera stream
+        video_capture = cv2.VideoCapture(0)
+    
+    etalon_encoding = np.load(path_to_etalon_encoding)
+    
+    ret, frame = video_capture.read()
+    frame = cv2.imread(photo_path)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    if len(face_encodings) != 0:
+        face_compares = face_recognition.compare_faces(face_encodings, etalon_face_encoding, tolerance=0.65)
+        if True in face_compares:
+            print(str(len(face_compares) + 'person/persons on image, one of them is the same, as etalon'))
+            return True
+        else:
+            print(str(len(face_compares) + 'person/persons on image, none of them is the same, as etalon'))
+            return False
+    else:
+        print('No faces were detected on camera!')
+        return False
+    
+
